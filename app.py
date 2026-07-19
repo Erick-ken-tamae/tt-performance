9bbf5af34d1748751893999304a409e15e31036a
Erick-ken-tamae <erickkentamae@gmail.com>
2026-07-19 18:40:10 -0300

    construindo o cadastro

--- app.py
+++ app.py
@@ -1,14 +1,37 @@
-from flask import Flask
-from database import conectar
+from flask import Flask, render_template, request, redirect
+from models.jogador import cadastrar_joagdor, listar_jogador
 
 
 app = Flask(__name__)
 
 @app.route("/")
 def inicio():
-    banco = conectar()
-    
-    return "Banco Conectado com sucesso!"
+
+    jogadores = listar_jogador()
+
+    return render_template(
+        "cadastro_jogador.html",
+        jogadores=jogadores
+    )
+
+
+@app.route("/cadastrar", methods=["POST"])
+def cadastrar():
+
+    nome = request.form["nome"]
+    idade = request.form["idade"]
+    categoria = request.form["categoria"]
+    clube = request.form["clube"]
+
+    cadastrar(
+        nome,
+        idade,
+        categoria,
+        clube
+    )
+
+    return redirect("/")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ models/jogador.py
@@ -0,0 +1,34 @@
+from database import conectar
+
+def cadastrar_jogador(nome, idade, categoria, clube):
+    banco = conectar()
+    cursor = banco.cursor()
+    
+    sql= """
+        INSERT INTO jogador
+        (nome, idade, categoria, clube)
+        VALUES
+        (%s,%s,%s,%s)
+    """
+    
+    valores=(nome, idade, categoria, clube)
+    
+    cursor.execute(sql, valores)
+    
+    banco.commit()
+    cursor.close()
+    banco.close()
+
+def listar_jogador():
+    banco = conectar()
+    cursor = banco.cursor(dictionary=True)
+    
+    cursor.execute("SELECT * FROM")
+    
+    jogadores = cursor.fetchall()
+    
+    cursor.close()
+    banco.close()
+    
+    return jogadores
+    
