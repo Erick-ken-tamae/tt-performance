d16f506619a2e721dacc80ad01b25b7cad78d9c5
Erick-ken-tamae <erickkentamae@gmail.com>
2026-08-01 22:28:38 -0300

    mudando a rota

--- database.py
+++ database.py
@@ -1,11 +1,13 @@
+import os
 import mysql.connector
 
 def conectar():
-    banco =  mysql.connector.connect(
-        host="localhost",
-        user = "root",
-        password = "1005yukio",
-        database="tt_performance"
+    banco = mysql.connector.connect(
+        host=os.environ.get("DB_HOST", "localhost"),
+        user=os.environ.get("DB_USER", "root"),
+        password=os.environ.get("DB_PASSWORD", "1005yukio"),
+        database=os.environ.get("DB_NAME", "tt_performance"),
+        port=int(os.environ.get("DB_PORT", 3306))
     )
-    
+
     return banco
